7b0ab4759f1a347d5dbd52b5ccab1d1ec12c11f4
Man Poor <poormanslaserscanner@gmail.com>
2017-11-22 13:56:54 +0100

    * Add matlab command line setup and startup messages

--- pmls_core/startup.m
+++ pmls_core/startup.m
@@ -1,4 +1,5 @@
 % Custom startup which loads last working directory and workspace
 %
 % See also: finish
-disp('Pmls is on the matlab path.');
+disp('Wellcome! Pmls is on the matlab path.');
+disp('Type "start" to start the matlab engine to which you can connect from our Blender add-on.');
--- setup.m
+++ setup.m
@@ -8,3 +8,5 @@ p = genpath('pmls_core');
 addpath(p);
 addpath('.');
 savepath
+disp('Wellcome! Pmls is on the matlab path.');
+disp('Add command "start" to start the matlab engine to which you can connect from our Blender add-on.');
