75af6436ccead4f129254ddb94425866e1b39f91
Man Poor <poormanslaserscanner@gmail.com>
2018-11-04 09:49:22 +0100

    Add: new parameter 'shorten' in function 'plvoxunionhdgs'

--- pmls_core/plvoxuniohdgs.m
+++ pmls_core/plvoxuniohdgs.m
@@ -1,7 +1,11 @@
-function S = plvoxuniohdgs( H, vox, ext, cuda, marcube )
+function S = plvoxuniohdgs( H, vox, ext, cuda, marcube, shorten )
 %PLVOXUNIOHDGS Summary of this function goes here
 %   Detailed explanation goes here
-[S.tris,S.vt] = remeshunion(H.base,H.erays,H.zeroshots, vox, ext, cuda, marcube, 0.7, {H.base, H.rays} );
+if nargin < 6
+    shorten = 0.7;
+end
+[S.tris,S.vt] = remeshunion(H.base,H.erays,H.zeroshots, vox, ext, cuda, marcube, shorten, {H.base, H.rays} );
+%[S.tris,S.vt] = remeshunion(H.base,H.erays,H.zeroshots, vox, ext, cuda, marcube, shorten, {} );
 S.hedgehog = H;
 S.pmls_type = 'deform_mesh';
 S.pmls_name = [H.pmls_name, '_union'];
